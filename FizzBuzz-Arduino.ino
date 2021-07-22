845a6c92a6b503b55916071a0ecb490e1ade07c5
hrtdx <58378109+hrtdx@users.noreply.github.com>
2021-07-22 19:11:30 +0700

    Update FizzBuzz-Arduino.ino

--- FizzBuzz-Arduino.ino
+++ FizzBuzz-Arduino.ino
@@ -1,25 +1,30 @@
 void setup() {
   Serial.begin(9600);
   Serial.println("Fizz Buzz on Arduino.");
-  
-  int count = 100;
-  for(int i=1; i<=count; i++){
-    if(i%3==0 && i%5==0){
-      Serial.println("Fizz Buzz");
-    }
-    else if(i%3==0){
-      Serial.println("Fizz");
-    }
-    else if(i%5==0){
-      Serial.println("Buzz");
-    }
-    else{
-      Serial.println(String(i));
-    }
-  }
+
+  fizzbuzz(100);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+void fizzbuzz(int i){
+  int count = i;
+  for(int a=1; a<=count; a++){
+    if(a%3==0 && a%5==0){
+      Serial.println("Fizz Buzz");
+    }
+    else if(a%3==0){
+      Serial.println("Fizz");
+    }
+    else if(a%5==0){
+      Serial.println("Buzz");
+    }
+    else{
+      Serial.println(String(a));
+    }
+  }
 }
